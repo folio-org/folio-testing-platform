943633a62048133cacc2c8455d1754c866dbb50e
Wolfram Schneider <wosch@indexdata.com>
2017-04-10 13:11:12 +0200

    new aws server

--- stripes.config.js
+++ stripes.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   okapi: { 
-	'url':'http://localhost:9130', 
+	//'url':'http://localhost:9130', 
+	'url':'http://ec2-34-204-15-75.compute-1.amazonaws.com:9130',
 	// 'url':'http://folio-ci10.aws.indexdata.com:9130', 
 	'tenant':'diku' 
   },
