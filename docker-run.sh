af4b441bed4708139f3a94ee02b1a78d696c5da5
Wolfram Schneider <wosch@indexdata.com>
2017-04-12 15:03:41 +0000

    listen on all interfaces for port forwarding

--- docker-run.sh
+++ docker-run.sh
@@ -6,5 +6,5 @@ set -e
 url=$(wget -q -O - 'http://folio-uidemo.aws.indexdata.com/bundle.js'|grep :9130| perl -ne 'print "$1\n" if m|url:(.*?),|')
 perl -i.bak -npe "s,url':'(.*?)',url':$url," stripes.config.js 
 
-yarn start
+STRIPES_HOST=0.0.0.0 yarn start
 
