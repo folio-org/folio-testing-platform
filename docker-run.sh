79e6d344f54c2803c068979ab0b8c6df59f752f7
Wolfram Schneider <wosch@indexdata.com>
2017-04-13 10:50:40 +0200

    configure network timeouts

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@
 set -e
 #set -x
 
-url=$(wget -q -O - 'http://folio-uidemo.aws.indexdata.com/bundle.js'|grep :9130| perl -ne 'print "$1\n" if m|url:(.*?),|')
+url=$(wget -q -O - --tries=1 --connect-timeout=5 --read-timeout=15 'http://folio-uidemo.aws.indexdata.com/bundle.js'|grep :9130| perl -ne 'print "$1\n" if m|url:(.*?),|')
 perl -i.bak -npe "s,url':'(.*?)',url':$url," stripes.config.js 
 
 STRIPES_HOST=0.0.0.0 yarn start
