dd3e7d9874b3fc357c76927699506cfa12bc3d7a
Viktor Soroka <Victor_Soroka1@epam.com>
2018-07-24 20:28:49 +0300

    Add ui-myprofile module.

--- stripes.config.js
+++ stripes.config.js
@@ -19,7 +19,8 @@ module.exports = {
     "@folio/vendors": {},
     "@folio/finance": {},
     "@folio/plugin-find-user" : {},
-    "@folio/tags": {}
+    "@folio/tags": {},
+    "@folio/myprofile": {}
   },
   branding: {
     logo: {
